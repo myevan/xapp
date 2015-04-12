8d2fd7cd0ca3c8e79cfce9d2158041a9c843990d
Song Young-jin <myevan80@gmail.com>
2015-04-12 14:34:30 +0900

    + on exception exit and ant debug install @ build_android

--- build_android.sh
+++ build_android.sh
@@ -1,4 +1,8 @@
+set -e
 pushd       __builds/android-armeabi-v7a
 make install VERBOSE=1
 popd
-cp __platforms/android-armeabi-v7a/lib/*.so projects/android_project/libs/armeabi-v7a/
+cp __platforms/android-armeabi-v7a/lib/*.so projects/$1/libs/armeabi-v7a/
+pushd       projects/$1
+ant debug install
+popd
