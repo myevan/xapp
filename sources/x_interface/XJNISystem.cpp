8ea095d4601ce04eb1cdbb33a778661d2fd46037
Song Young-jin <myevan80@gmail.com>
2015-04-12 14:52:40 +0900

    ! XJNISystem interface fixed

--- sources/x_interface/XJNISystem.cpp
+++ sources/x_interface/XJNISystem.cpp
@@ -1,17 +1,16 @@
 #include <x_interface/XJNIMacros.h>
-
-#include <x_foundation/XAndroidSystem.h>
 #include <x_foundation/XLogMacros.h>
+#include <x_foundation/XAndroidSystem.h>
 
 #include <jni.h>
 
 using namespace xf;
 
 extern "C" {
-    JNIEXPORT jstring JNICALL X_JNI_FUNCTION(XAndroidSystem, GetABIName)(JNIEnv* env, jobject thiz);
+    JNIEXPORT jstring JNICALL X_JNI_FUNCTION(XSystem, GetABIName)(JNIEnv* env, jobject thiz);
 };
 
-JNIEXPORT jstring JNICALL X_JNI_FUNCTION(XAndroidSystem, GetABIName)(JNIEnv* env, jobject thiz)
+JNIEXPORT jstring JNICALL X_JNI_FUNCTION(XSystem, GetABIName)(JNIEnv* env, jobject thiz)
 {
     x_debugn("test");
     const char* abiName = XAndroidSystem::GetABIName();
