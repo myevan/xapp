c6177825458799959967f976411d694b1107ffd7
Song Young-jin <myevan80@gmail.com>
2015-04-12 12:51:55 +0900

    ! projects renamed

--- rebuild_posix.sh
+++ rebuild_posix.sh
@@ -2,13 +2,13 @@ rm -rf      __builds/posix
 rm -rf      __platforms/posix
 mkdir -p    __platforms/posix
 mkdir -p    __builds/posix/framework
-mkdir -p    __builds/posix/projects/hello
+mkdir -p    __builds/posix/projects/hello_posix
 pushd       __builds/posix/framework
 cmake ../../.. -DCMAKE_INSTALL_PREFIX=../../../__platforms/posix -DPOSIX=1
 make install VEROBSE=1
 popd
-pushd       __builds/posix/projects/hello
-cmake ../../../../projects/posix_project -DCMAKE_INSTALL_PREFIX=../../../../__platforms/posix -DPOSIX=1
+pushd       __builds/posix/projects/hello_posix
+cmake ../../../../projects/hello_posix -DCMAKE_INSTALL_PREFIX=../../../../__platforms/posix -DPOSIX=1
 make install VERBOSE=1
 ./hello
 popd
