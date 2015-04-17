9e1ec01ac866d17d35b58432183f961c7c4fe150
Song Young-jin <myevan80@gmail.com>
2015-04-18 00:12:11 +0900

    + XFileStream

---
+++ sources/x_foundation/XFileStream.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#ifndef __X_FILE_STREAM__
+#define __X_FILE_STREAM__
+
+#include <x_foundation/XTypes.h>
+
+namespace xf {
+
+class XFileStream
+{
+public:
+    XFileStream()
+    : m_filePtr(NULL)
+    {
+    }
+
+    virtual ~XFileStream()
+    {
+        Close();
+    }
+
+    void Close()
+    {
+        if (m_filePtr != NULL)
+        {
+            fclose(m_filePtr);
+            m_filePtr = NULL;
+        }
+    }
+
+    bool Open(const XString& fileAbsPath)
+    {
+        m_filePtr = fopen(fileAbsPath.GetPtr(), "rb");
+        if (!m_filePtr)
+            return false;
+
+        return true;
+    }
+
+    size_t GetSize()
+    {
+        size_t curPos = ftell(m_filePtr);
+        fseek(m_filePtr, 0, SEEK_END);
+        size_t endPos = ftell(m_filePtr);
+        fseek(m_filePtr, curPos, SEEK_SET);
+        return endPos;
+    }
+
+    size_t ReadBytes(size_t byteCount, byte_t* outBytes)
+    {
+        return fread(outBytes, 1, byteCount, m_filePtr);
+    }
+
+private:
+    FILE* m_filePtr;
+};
+
+} // end_of_namespace:xf
+
+#endif
