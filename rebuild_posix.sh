305f9beb6b7a20b462383079ea1a5bfcabdf4ebe
Song Young-jin <myevan80@gmail.com>
2015-04-18 02:43:44 +0900

    ! rebuild_posix definition fixed

--- rebuild_posix.sh
+++ rebuild_posix.sh
@@ -4,9 +4,9 @@ mkdir -p    __platforms/posix
 mkdir -p    __builds/posix/framework
 mkdir -p    __builds/posix/projects/$1
 pushd       __builds/posix/framework
-cmake ../../.. -DCMAKE_INSTALL_PREFIX=../../../__platforms/posix -DPOSIX=1
+cmake ../../.. -DCMAKE_INSTALL_PREFIX=../../../__platforms/posix -DFORCE_POSIX=1
 popd
 pushd       __builds/posix/projects/$1
-cmake ../../../../projects/$1 -DCMAKE_INSTALL_PREFIX=../../../../__platforms/posix -DPOSIX=1
+cmake ../../../../projects/$1 -DCMAKE_INSTALL_PREFIX=../../../../__platforms/posix -DFORCE_POSIX=1
 popd
 ./build_posix.sh $1
