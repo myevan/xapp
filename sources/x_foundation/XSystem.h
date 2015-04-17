b4c97474202ff471ae5454d4a4e8b9164bdb09c3
Song Young-jin <myevan80@gmail.com>
2015-04-18 00:16:05 +0900

    + XSystem::LoadFileBinary

--- sources/x_foundation/XSystem.h
+++ sources/x_foundation/XSystem.h
@@ -4,10 +4,12 @@
 #define __X_SYSTEM__
 
 #include <x_foundation/XURI.h>
+#include <x_foundation/XPool.h>
 #include <x_foundation/XString.h>
-#include <x_foundation/XBinary.h>
+#include <x_foundation/XFileBinary.h>
 
 #include <string>
+#include <memory>
 
 namespace xf { 
 
@@ -23,6 +25,25 @@ public:
     virtual bool TryGetResourceAbsPath(const XString& dirRelPath, const XString& fileName, std::string& outAbsPath) { return false; }
 
     virtual XBinary* TryGetResourceBinary(const XString& dirRelPath, const XString& fileName) { return NULL; }
+
+    std::shared_ptr<XBinary> LoadFileBinary(const XString& fileAbsPath)
+    {
+        std::shared_ptr<XFileStream> newStreamPtr = XPool<XFileStream>::NewObject();
+        XFileStream& newStream = *newStreamPtr;
+        if (!newStream.Open(fileAbsPath))
+            return std::shared_ptr<XFileBinary>();
+
+        size_t newStreamSize = newStream.GetSize();
+        std::shared_ptr<XFileBinary> newBinPtr = XPool<XFileBinary>::NewObject(newStreamSize);
+        
+        XFileBinary& newBin = *newBinPtr;
+        newBin.SetFilePath(fileAbsPath);
+        if (!newBin.TryLoadFile(newStream))
+            return std::shared_ptr<XFileBinary>();
+
+        return newBinPtr;
+    }
+
 };
 
 } // end_of_namespace:xf
