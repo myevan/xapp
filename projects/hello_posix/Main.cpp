c7d88f3bc2e99cc95aeb5063aaff4a11a8421272
Song Young-jin <myevan80@gmail.com>
2015-04-18 23:22:32 +0900

    + hello_posix made simple

--- projects/hello_posix/Main.cpp
+++ projects/hello_posix/Main.cpp
@@ -1,14 +1,11 @@
 #include "StdAfx.h"
 
-#include <x_foundation/XLogMacros.h>
-#include <x_foundation/XBaseConfig.h>
+#include <x_foundation/XDebugMacros.h>
 
 using namespace xf;
 
 int main(int argc, const char** argv)
 {
-    XBaseConfig& baseConfig = XBaseConfig::GetBaseConfig();
-    const XString& title = baseConfig.GetTitle();
-    x_debugn(title);
+    x_debug("Hello World");
     return 0;
 }
