e9ecc7410135d60cae3eaac13d611c097789e55e
Song Young-jin <myevan80@gmail.com>
2015-04-19 14:01:30 +0900

    + rebuild_android ANDROID_PROJECT_DIR

--- rebuild_android.sh
+++ rebuild_android.sh
@@ -3,6 +3,6 @@ rm -rf      __platforms/android-armeabi-v7a
 mkdir -p    __platforms/android-armeabi-v7a
 mkdir -p    __builds/android-armeabi-v7a
 pushd       __builds/android-armeabi-v7a
-cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../../toolchains/android.cmake -DANDROID_NDK=$NDK_HOME -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=armeabi-v7a -DCMAKE_INSTALL_PREFIX=../../__platforms/android-armeabi-v7a -DJNI_PACKAGE_NAME=x_framework -DANDROID_NATIVE_API_LEVEL=android-15
+cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../../toolchains/android.cmake -DANDROID_NDK=$NDK_HOME -DANDROID_ABI=armeabi-v7a -DCMAKE_INSTALL_PREFIX=../../__platforms/android-armeabi-v7a -DANDROID_NATIVE_API_LEVEL=android-15 -DJNI_PACKAGE_NAME=x_framework -DCMAKE_BUILD_TYPE=Debug -DANDROID_PROJECT_DIR=../../projects/$1
 popd
 ./build_android.sh $1
