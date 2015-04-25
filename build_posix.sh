452055883a8f9e3f0aee36267e3618991b63e38e
Song Young-jin <myevan80@gmail.com>
2015-04-25 12:42:55 +0900

    ! posix link bug fixed

--- build_posix.sh
+++ build_posix.sh
@@ -2,6 +2,7 @@ pushd       __builds/posix/framework
 make install VEROBSE=1
 popd
 pushd       __builds/posix/projects/$1
+make clean
 make install VERBOSE=1
 popd
 pushd       __platforms/posix/bin
